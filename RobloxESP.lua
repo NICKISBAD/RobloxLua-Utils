d90eab259a504e9b11a3bf23673de246cdb37e39
Nick <107813557+NICKISBAD@users.noreply.github.com>
2023-08-08 12:12:09 -0700

    Update RobloxESP.lua

--- RobloxESP.lua
+++ RobloxESP.lua
@@ -1,18 +1,51 @@
-local function CreateEsp(Targ, Name)
-    local bb = Instance.new("BillboardGui")
-    bb.Name = "EspBillboard"
-    bb.Parent = Targ
-    
-    local text = Instance.new("TextLabel", bb)
-    text.Name = "EspLabel"
-    text.Text = Name .. ":"
-    
-    local function UpdateDistance()
-        while wait() do
-            local distance = (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - Targ.Character.HumanoidRootPart.Position).Magnitude
-            text.Text = Name .. ": " .. distance
+local RunService = game:GetService("RunService")
+
+local function CreateESP(Name,Part,Size)
+    local player = game.Players.LocalPlayer
+    local camera = game:GetService("Workspace").CurrentCamera
+
+    local esp = Instance.new("BillboardGui")
+    esp.Name = "esp"
+    esp.Size = UDim2.new(0, 200, 0, 50)
+    esp.StudsOffset = Vector3.new(0, 1.5, 0) -- Offset to raise the label slightly
+    esp.Adornee = Part
+    esp.Parent = Part
+    esp.AlwaysOnTop = true
+
+
+    local esplabelfr = Instance.new("TextLabel")
+    esplabelfr.Name = "esplabelfr"
+    esplabelfr.Size = UDim2.new(1, 0, 0, 70)
+    esplabelfr.BackgroundColor3 = Color3.new(0, 0, 0)
+    esplabelfr.TextColor3 = Color3.fromRGB(255,255,255)
+    esplabelfr.BackgroundTransparency = 1
+    esplabelfr.TextStrokeTransparency = 0
+    esplabelfr.TextStrokeColor3 = Color3.new(0, 0, 0)
+    esplabelfr.TextSize = Size
+    esplabelfr.TextScaled = false
+    esplabelfr.Parent = esp
+
+    local function updateesplabelfr()
+        local playerPosition = player.Character and player.Character:FindFirstChild("HumanoidRootPart")
+        if playerPosition then
+            local distance = (playerPosition.Position - Part.Position).Magnitude
+            esplabelfr.Text = string.format(Name .. ": %.2f", distance)
+            
+            local headPosition = Part.Position + Vector3.new(0, Part.Size.Y / 2, 0)
+            local screenPosition, onScreen = camera:WorldToScreenPoint(headPosition)
+            
+            if onScreen or playerPosition.Position.Y > Part.Position.Y then
+                esp.Adornee = Part
+                esp.Enabled = true
+            else
+                esp.Enabled = false
+            end
+        else
+            esp.Enabled = false
         end
     end
-    
-    coroutine.wrap(UpdateDistance)()
+
+    RunService.RenderStepped:Connect(updateesplabelfr)
 end
+
+CreateESP("PartName" , game.Workspace.Part , 10)
